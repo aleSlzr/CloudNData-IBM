8bda1fc94e5b1de69be21ddf134fa78806a37881
Alejandro Salazar <ascruz@mx1.ibm.com>
2019-10-14 22:57:25 -0500

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default() => {
   return axios.create({
-    baseURL: process.env.API_URL //`http://localhost:8081`
+    baseURL: `https://ibm-music-server.mybluemix.net/`//`http://localhost:8081`
   })
 }
